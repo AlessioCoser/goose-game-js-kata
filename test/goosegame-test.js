e10a0634169e3f349fb8cf0a3fc966181086ea9a
alessiocoser <alessio.coser@xpeppers.com>
2017-02-21 14:32:07 +0100

    move a player from previous position

--- test/goosegame-test.js
+++ test/goosegame-test.js
@@ -29,4 +29,13 @@ test('GooseGame', function () {
     var moveResponse = game.movePlayer('Pippo', 4, 2)
     equal(moveResponse, 'Pippo tira 4, 2. Pippo muove da Partenza a 6')
   })
+
+  test('move a player from previous position', function () {
+    let game = new GooseGame()
+    game.addPlayer('Pippo')
+    game.movePlayer('Pippo', 2, 3)
+
+    var moveResponse = game.movePlayer('Pippo', 2, 2)
+    equal(moveResponse, 'Pippo tira 2, 2. Pippo muove da 5 a 9')
+  })
 })
