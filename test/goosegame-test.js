30789a6765fd0bd835011d92252eb95627247257
alessiocoser <alessio.coser@xpeppers.com>
2017-02-21 14:20:56 +0100

    message error when player already exists

--- test/goosegame-test.js
+++ test/goosegame-test.js
@@ -7,10 +7,18 @@ test('GooseGame', function () {
 
     equal(game.addPlayer('Pippo'), 'Giocatori: Pippo')
   })
+
   test('add another player to game', function () {
     let game = new GooseGame()
     game.addPlayer('Pippo')
 
     equal(game.addPlayer('Pluto'), 'Giocatori: Pippo, Pluto')
+  })
+
+  test('message error when player already exists', function () {
+    let game = new GooseGame()
+    game.addPlayer('Pippo')
+
+    equal(game.addPlayer('Pippo'), 'Pippo: Giocatore già presente')
   })
 })
