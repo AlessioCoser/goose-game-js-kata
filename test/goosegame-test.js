be10c1112dbce8d4e69295722c7a9334edd1ebe3
alessiocoser <alessio.coser@xpeppers.com>
2017-02-22 18:03:37 +0100

    use dicePair instead of 2 different variables

--- test/goosegame-test.js
+++ test/goosegame-test.js
@@ -26,42 +26,42 @@ test('GooseGame', function () {
     let game = new GooseGame()
     game.addPlayer('Pippo')
 
-    var moveResponse = game.movePlayer('Pippo', 4, 2)
+    var moveResponse = game.movePlayer('Pippo', [4, 2])
     equal(moveResponse, 'Pippo tira 4, 2. Pippo muove da Partenza a 6')
   })
 
   test('move a player from previous position', function () {
     let game = new GooseGame()
     game.addPlayer('Pippo')
-    game.movePlayer('Pippo', 2, 3)
+    game.movePlayer('Pippo', [2, 3])
 
-    var moveResponse = game.movePlayer('Pippo', 2, 2)
+    var moveResponse = game.movePlayer('Pippo', [2, 2])
     equal(moveResponse, 'Pippo tira 2, 2. Pippo muove da 5 a 9')
   })
 
   test('player wins the game when reaches 63', function () {
     let game = new GooseGame()
     game.addPlayer('Pippo')
-    game.movePlayer('Pippo', 6, 6)
-    game.movePlayer('Pippo', 6, 6)
-    game.movePlayer('Pippo', 6, 6)
-    game.movePlayer('Pippo', 6, 6)
-    game.movePlayer('Pippo', 6, 6)
+    game.movePlayer('Pippo', [6, 6])
+    game.movePlayer('Pippo', [6, 6])
+    game.movePlayer('Pippo', [6, 6])
+    game.movePlayer('Pippo', [6, 6])
+    game.movePlayer('Pippo', [6, 6])
 
-    var moveResponse = game.movePlayer('Pippo', 2, 1)
+    var moveResponse = game.movePlayer('Pippo', [2, 1])
     equal(moveResponse, 'Pippo tira 2, 1. Pippo muove da 60 a 63. Pippo vince!!')
   })
 
   test('player bounces when player exceeds 63', function () {
     let game = new GooseGame()
     game.addPlayer('Pippo')
-    game.movePlayer('Pippo', 6, 6)
-    game.movePlayer('Pippo', 6, 6)
-    game.movePlayer('Pippo', 6, 6)
-    game.movePlayer('Pippo', 6, 6)
-    game.movePlayer('Pippo', 6, 6)
+    game.movePlayer('Pippo', [6, 6])
+    game.movePlayer('Pippo', [6, 6])
+    game.movePlayer('Pippo', [6, 6])
+    game.movePlayer('Pippo', [6, 6])
+    game.movePlayer('Pippo', [6, 6])
 
-    var moveResponse = game.movePlayer('Pippo', 2, 3)
+    var moveResponse = game.movePlayer('Pippo', [2, 3])
     equal(moveResponse, 'Pippo tira 2, 3. Pippo muove da 60 a 63. Pippo rimbalza! Pippo torna a 61')
   })
 })
