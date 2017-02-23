f056ec180b26986a5a07e8c49244a68c0e432338
alessiocoser <alessio.coser@xpeppers.com>
2017-02-23 14:45:49 +0100

    add ugly code

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-console.log("hi!")
+const CommandBus = require('./lib/commands')
+const GooseGame = require('./lib/goosegame')
+
+class Cli {
+  constructor (prompt = require('prompt')) {
+    this._prompt = prompt
+    this._game = new GooseGame()
+    this._commandBus = new CommandBus(this._game)
+    this._prompt.start()
+  }
+
+  runPrompt () {
+    this._prompt.get(['command'], this._runCommand(this.runPrompt.bind(this)))
+  }
+
+  _runCommand (callback) {
+    return (err, result) => {
+      if (err) {
+        return 1
+      }
+
+      let response = this._commandBus.execute(result.command)
+
+      if (response) {
+        console.log(response)
+      }
+
+      if(this._game.ended()) {
+        this._commandBus.execute("exit")
+      }
+
+      callback()
+    }
+  }
+}
+
+let cli = new Cli()
+cli.runPrompt()
