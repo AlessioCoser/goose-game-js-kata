e0c0ecf00380ce41b8a926371f85424138029e08
alessiocoser <alessio.coser@xpeppers.com>
2017-03-06 14:44:06 +0100

    add cli

--- index.js
+++ index.js
@@ -1,38 +1,4 @@
-const CommandBus = require('./lib/commands')
-const GooseGame = require('./lib/goosegame')
+const CliGame = require('./lib/goose-game-cli')
 
-class Cli {
-  constructor (prompt = require('prompt')) {
-    this._prompt = prompt
-    this._game = new GooseGame()
-    this._commandBus = new CommandBus(this._game)
-    this._prompt.start()
-  }
-
-  runPrompt () {
-    this._prompt.get(['command'], this._runCommand(this.runPrompt.bind(this)))
-  }
-
-  _runCommand (callback) {
-    return (err, result) => {
-      if (err) {
-        return 1
-      }
-
-      let response = this._commandBus.execute(result.command)
-
-      if (response) {
-        console.log(response)
-      }
-
-      if(this._game.ended()) {
-        this._commandBus.execute("exit")
-      }
-
-      callback()
-    }
-  }
-}
-
-let cli = new Cli()
-cli.runPrompt()
+let game = new CliGame()
+game.start()
