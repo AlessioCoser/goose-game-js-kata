f50f4329d06919a57b381244ce791cee5ee43e92
Alessio Coser <alessio.coser@xpeppers.com>
2018-01-22 11:37:50 +0100

    adds players

--- test/goose-game.js
+++ test/goose-game.js
@@ -1,9 +1,19 @@
 const { equal } = require('assert')
 const GooseGame = require('../lib/goose-game')
 
-test('adds first player', () => {
-  var game = new GooseGame()
-  var response = game.send('add player Pippo')
+test('GooseGame', () => {
+  test('adds first player', () => {
+    var game = new GooseGame()
+    var response = game.send('add player Pippo')
 
-  equal(response, 'players: Pippo')
+    equal(response, 'players: Pippo')
+  })
+
+  test('adds two players player', () => {
+    var game = new GooseGame()
+    game.send('add player Pippo')
+    var response = game.send('add player Pluto')
+
+    equal(response, 'players: Pippo, Pluto')
+  })
 })
